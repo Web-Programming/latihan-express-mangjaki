6584868c2169f397fc3c938af76d815f8250c20c
mangjaki <cungcang996@gmail.com>
2024-10-11 16:39:47 +0700

    update
    11 oktober

---
+++ express-mvc/app_server/controllers/main.js
@@ -0,0 +1,10 @@
+const index = (req, res) => {
+ 	res.render('index', { title: 'Express' }); 
+};
+
+//buat controller untuk halaman kontak
+const kontak = (req, res) => {
+ 	res.render('kontak', { title: 'Kontak' }); 
+};
+
+module.exports = { index , kontak};
